27abdb67f19d70ea0e7893e1a0dfecc9f64ace66
mdarolon <71890425+mdarolon@users.noreply.github.com>
2021-02-04 09:56:07 -0300

    Delete exportacion directory

--- exportacion/sql_exportacion/ScriptA_carreras.sql
+++
@@ -1,8 +0,0 @@
--- Script A
--- Exportacion de las carreras (sga_carreras)
--- BD: SIU-Guarani
-
-select unidad_academica, carrera, nombre
- from sga_carreras;
-
-
--- exportacion/sql_exportacion/ScriptD_colaciones.sql
+++
@@ -1,7 +0,0 @@
--- Script D
--- Exportacion de las colaciones (sga_colaciones)
--- BD: SIU-Guarani
-
-select nro_colacion, descripcion, anio colacion 
-from sga_colaciones;
-
